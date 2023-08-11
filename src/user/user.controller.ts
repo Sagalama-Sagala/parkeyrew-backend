6a7ea3404978796c697026faa82030e9105f692d
hon <honhonhonx312@gmai.com>
2023-08-11 18:23:51 +0700

    ChangeDTo

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,28 @@
 import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './schemas/user.schema';
+import { getUserDto } from './dto/get-user.dto';
 import { createUserDto } from './dto/create-user.dto';
 import { updateUserDto } from './dto/update-user.dto';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
-  constructor(private userService: UserService) {}
+  constructor(private readonly userService: UserService) {} // Inject the user service
 
   @Get()
-  async getAllUsers(): Promise<User[]> {
-    return this.userService.findAll();
+  async getUsers(): Promise<getUserDto[]> {
+    // Fetch users from the service
+    const users: any[] = await this.userService.getUsers();
+
+    // Map users to getUserDto instances
+    const userDtos: getUserDto[] = users.map(user => ({
+      username: user.username,
+      displayName: user.displayName,
+      tel: user.tel,
+      email: user.email,
+    }));
+
+    return userDtos;
   }
 
   @Get(':id')
