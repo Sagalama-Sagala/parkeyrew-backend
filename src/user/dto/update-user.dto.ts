7a42df114af4adbe0faeadeb22d056a3918fa297
กฤตยชญ์ แสงงาม <hon@Macintosh.local>
2023-09-22 18:52:02 +0700

    dev

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,24 +1,28 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString,Matches } from 'class-validator';
 
 export class updateUserDto {
   @ApiProperty()
   @IsString()
   @IsNotEmpty()
+  @Matches(/[a-zA-Z0-9_-]{2,20}/)
   readonly username: string;
 
   @ApiProperty()
   @IsString()
   @IsNotEmpty()
+  @Matches(/^\$2[ayb]\$[\d]{2}\$[./A-Za-z0-9]{53}$/)
   readonly password: string;
 
   @ApiProperty()
   @IsString()
   @IsNotEmpty()
+  @Matches(/[a-zA-Z0-9_-]{2,20}/)
   readonly displayName: string;
 
   @ApiProperty()
   @IsString()
+  @Matches(/[0-9_-]{10}/)
   readonly tel: string;
 
   @ApiProperty()
