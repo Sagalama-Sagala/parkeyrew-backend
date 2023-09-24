28d51f51d54696061772065ea22514ec81e4cef5
Bourbxn <jakkapat97@gmail.com>
2023-09-24 21:09:40 +0700

    init dev

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, IsString,Matches } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, Matches } from 'class-validator';
 
 export class updateUserDto {
   @ApiProperty()
