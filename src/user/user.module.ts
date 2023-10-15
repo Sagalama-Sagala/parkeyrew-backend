5473f9a76a5b3384a0f05ddf6f8dbbed2b9e4f71
Bourbxn <jakkapat97@gmail.com>
2023-10-15 22:02:59 +0700

    update profile image upload

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -5,12 +5,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './schemas/user.schema';
 import { ProductModule } from '../product/product.module';
 import { AddressModule } from 'src/address/address.module';
+import { FileUploadModule } from 'src/file-upload/file-upload.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
     forwardRef(() => ProductModule),
     forwardRef(() => AddressModule),
+    FileUploadModule,
   ],
   controllers: [UserController],
   providers: [UserService],
