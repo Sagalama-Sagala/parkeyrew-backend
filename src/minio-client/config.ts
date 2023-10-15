2272ce93bd624587299aee0eea25e0b79878caa5
กฤตยชญ์ แสงงาม <hon@Macintosh.local>
2023-10-15 18:26:33 +0700

    minio

--- src/minio-client/config.ts
+++ src/minio-client/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  MINIO_ENDPOINT: 'localhost',
+  MINIO_ENDPOINT: '10.128.0.5',
   MINIO_PORT: 8006,
   MINIO_ACCESS_KEY: 'y5ZDcs6hJhUsdZj5PkNI',
   MINIO_SECRET_KEY: 'zL9Rc8WJpAUkzR846OXRleyDTbC66oDIYIwOJDXu',
