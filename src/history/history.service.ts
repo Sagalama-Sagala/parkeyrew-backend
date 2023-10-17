7914d2e1860e02270d6331fbb7ce8ec91d14cd06
ktyjapz <katanyoo8945@gmail.com>
2023-10-17 22:02:31 +0700

    random

--- src/history/history.service.ts
+++ src/history/history.service.ts
@@ -15,14 +15,16 @@ export class HistoryService {
 
     async findByShop(userId: string): Promise<History[]> {
         return await this.HistoryModel.find({ shop: userId })
-        .populate({ path: "products", select: "name price deliveryFee" })
+        .populate({ path: "product", select: "name price deliveryFee" })
         .populate("review");
     }
 
     async findByCustomer(userId: string): Promise<History[]> {
-        return await this.HistoryModel.find({ customer: userId })
+        const res =  await this.HistoryModel.find({ customer: userId })
         .populate({ path: "products", select: "name price deliveryFee" })
         .populate("review");
+        console.log(res);
+        return res;
     }
 
     async create(product: Product, shop: User, customer: User): Promise<any>{
