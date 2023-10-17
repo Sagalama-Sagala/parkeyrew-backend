1b0b2d79e15afccdbc715c66108c377772f6730d
Bourbxn <jakkapat97@gmail.com>
2023-10-18 04:10:10 +0700

    update dto room

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,42 +1,50 @@
-import { Body, Controller, Get, Param, Post, Req } from "@nestjs/common";
-import { ReviewService } from "./review.service"; 
-import { ApiOkResponse, ApiCreatedResponse, ApiTags, ApiBadRequestResponse } from "@nestjs/swagger";
-import { Review } from "./schemas/review.schema";
-import { createReviewDto } from "./dto/create-review.dto";
+import { Body, Controller, Get, Param, Post, Req } from '@nestjs/common';
+import { ReviewService } from './review.service';
+import {
+  ApiOkResponse,
+  ApiCreatedResponse,
+  ApiTags,
+  ApiBadRequestResponse,
+} from '@nestjs/swagger';
+import { Review } from './schemas/review.schema';
+import { createReviewDto } from './dto/create-review.dto';
 
 @ApiTags('Review')
 @Controller('review')
-export class ReviewController{
-    constructor(private readonly reviewService: ReviewService){};
+export class ReviewController {
+  constructor(private readonly reviewService: ReviewService) {}
 
-    @ApiOkResponse({
-        description: 'Get review by userId successfully',
-    })
-    @Get('get-review-by-userId')
-    async getReviewByUserId(@Req() req:any): Promise<Review[]>{
-        return await this.reviewService.findAllByShopId(req.userId);
-    }
+  @ApiOkResponse({
+    description: 'Get review by userId successfully',
+  })
+  @Get('get-review-by-userId')
+  async getReviewByUserId(@Req() req: any): Promise<Review[]> {
+    return await this.reviewService.findAllByShopId(req.userId);
+  }
 
-    @ApiOkResponse({
-        description: 'Get review by shopId successfully',
-    })
-    @Get('get-review-by-shopId/:id')
-    async getReviewByShopId(@Param('id') id: string): Promise<Review[]>{
-        return await this.reviewService.findAllByShopId(id);
-    }
+  @ApiOkResponse({
+    description: 'Get review by shopId successfully',
+  })
+  @Get('get-review-by-shopId/:id')
+  async getReviewByShopId(@Param('id') id: string): Promise<Review[]> {
+    return await this.reviewService.findAllByShopId(id);
+  }
 
-    @ApiCreatedResponse({
-        description: 'Create review by userId successfully',
-        type: Review,
-    })
-    @ApiBadRequestResponse({
-        description: 'Cannot create review. Try again',
-    })
-    @Post('create-review')
-    async createReview(@Req() req: any,@Body() review: createReviewDto): Promise<Review>{
-        const newReview = await this.reviewService.create(req.userId, review);
-        newReview.shop.password = "";
-        newReview.customer.password = "";
-        return newReview
-    }
+  @ApiCreatedResponse({
+    description: 'Create review by userId successfully',
+    type: Review,
+  })
+  @ApiBadRequestResponse({
+    description: 'Cannot create review. Try again',
+  })
+  @Post('create-review')
+  async createReview(
+    @Req() req: any,
+    @Body() review: createReviewDto,
+  ): Promise<Review> {
+    const newReview = await this.reviewService.create(req.userId, review);
+    newReview.shop.password = '';
+    newReview.customer.password = '';
+    return newReview;
+  }
 }
